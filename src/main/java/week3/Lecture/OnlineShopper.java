60e52614a0edd16a6614a454f373078fe0ee9ad3
Hassanal <u2102848@siswa.um.edu.my>
2022-03-29 16:48:59 +0800

    Lecture week 3

---
+++ src/main/java/week3/Lecture/OnlineShopper.java
@@ -0,0 +1,7 @@
+package week3.Lecture;
+
+public class OnlineShopper {
+    public static void main(String[] args) {
+//        Item [] items = {};
+    }
+}
---
+++ src/main/java/week3/Spectrum/PiggyBank/CoinName.java
@@ -0,0 +1,2 @@
+package week3.Spectrum.PiggyBank;
+public enum CoinName {PENNY, NICKEL, DIME, QUARTER, FIFTY_CENT, DOLLAR}
