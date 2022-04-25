2fb817f423c933799614b1aac1c07300fc356748
Hassanal <u2102848@siswa.um.edu.my>
2022-04-25 22:59:04 +0800

    idk

--- src/main/java/week7/spectrum/PriorityQueue2.java
+++ src/main/java/week7/spectrum/PriorityQueue2.java
@@ -4,14 +4,17 @@ import java.util.*;
 
 public class PriorityQueue2 {
   public static void main(String[] args) {
+	  // higher value higher priority
 	PriorityQueue<Customer> customerQueue
 	  = new PriorityQueue<>(Collections.reverseOrder());
 
+	// sort by id not name
 	customerQueue.add(new Customer(3, "Donald" ));
 	customerQueue.add(new Customer(1, "Chong" ));
 	customerQueue.add(new Customer(2, "Ali" ));
 	customerQueue.add(new Customer(4, "Bala" ));
- 
+
+	// will return the highest priority without removing it from queue
 	Customer c = customerQueue.peek();
 	if (c!=null) {
 		System.out.println(c.getName() + " is in queue");
--- src/main/java/week7/spectrum/PriorityQueueDemo.java
+++ src/main/java/week7/spectrum/PriorityQueueDemo.java
@@ -5,30 +5,35 @@ import java.util.*;
 public class PriorityQueueDemo {
   public static void main(String[] args) {
     System.out.println("------------------------------");
-
+      // basically when use the empty constructor
+      // the smallest value has the highest priority
     PriorityQueue<String> queue1 = new PriorityQueue<>();
+    // input order not important bcs comparable will do the job
     queue1.offer("Oklahoma");
     queue1.offer("Indiana");
     queue1.offer("Georgia");
     queue1.offer("Texas");
-    System.out.println(queue1.comparator());
-    System.out.println(queue1.iterator());
+//    System.out.println(queue1.comparator());
+//    System.out.println(queue1.iterator());
     System.out.println("Priority queue using Comparable:");
 
     while (queue1.size() > 0) {
-      System.out.print(queue1.poll() + " ");
+        // since georgia is smallest so smallest has the highest priority
+      System.out.print(queue1.poll() + " "); // removes one with the highest priority
     }
 
 System.out.println("\n====================================");
-    PriorityQueue<String> queue2 
+//      since here have parameters 4 and Collections.reverseOrder()
+//      this queue will be the highest value is the highest priority
+    PriorityQueue<String> queue2
       = new PriorityQueue<>(4, Collections.reverseOrder());
     queue2.offer("Oklahoma");
     queue2.offer("Indiana");
     queue2.offer("Indiana");
     queue2.offer("Georgia");
     queue2.offer("Texas");
-        System.out.println(queue2.comparator());
-        System.out.println(queue2.iterator());
+//        System.out.println(queue2.comparator());
+//        System.out.println(queue2.iterator());
     System.out.println("\nPriority queue using Comparator:");
     while (queue2.size() > 0) {
         System.out.print(queue2.poll() + " ");
