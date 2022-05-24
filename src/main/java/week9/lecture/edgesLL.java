7230279a1cc4d78f10a568079787a67eeb8fd52c
Hassanal <u2102848@siswa.um.edu.my>
2022-05-24 18:43:27 +0800

    lecture week 9 graphs

--- src/main/java/week9/lecture/edgesLL.java
+++ src/main/java/week9/lecture/edgesLL.java
@@ -1,8 +1,14 @@
 package week9.lecture;
 
 public class edgesLL <T extends Comparable<T> , N extends Comparable<N>>{
+
+    // basically the destination vertex of the edge
+    // because each edge must have a source so the source is in the vertexLL class
     vertexLL<T, N> destination;
+    // weight of the edge itself
     N weight;
+    // this will point to the next edge if present
+    // like a list , this will help form the adjacency list
     edgesLL<T, N> nextEdge;
 
     public edgesLL() {
--- src/main/java/week9/lecture/vertexLL.java
+++ src/main/java/week9/lecture/vertexLL.java
@@ -1,10 +1,16 @@
 package week9.lecture;
 
 public class vertexLL <T extends Comparable<T>, N extends Comparable<N>>{
+    // this variable will store what each vertex node actually is
     T vertexObject;
+    // in deg is how many edges are coming into the node
     int indeg;
+    // out deg is how many edges are coming out of the node
     int outdeg;
+    // will point to the next vertex if available
+    // will help in creating the linked structure of a graph
     vertexLL<T, N> nextVertex;
+    // stores the first edge information ie the destination vertex of the source vertex (vertexObject)
     edgesLL<T, N> firstEdge;
 
     public vertexLL(){
